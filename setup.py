3f6386ea9516d3d6ddbfa5a705136913c6fe0746
Sean Bleier <sebleier@gmail.com>
2010-09-29 17:57:10 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name = "django-weathertracking",
+    url = "http://github.com/adamfast/django-weathertracking",
+    author = "Adam Fast",
+    author_email = "adamfast@gmail.com",
+    version = "0.1",
+    license = "BSD",
+    packages = ["weathertracking"],
+    description = "App for getting current conditions.",
+    classifiers = [
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+    ],
+)
